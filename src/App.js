7925b4462fbe32b1302ae3af5ac326bfbef5f9d7
kishor-karthik-dev <18mech094@gmail.com>
2024-01-29 19:23:22 +0530

    usestate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState}from "react";
 
 function App() {
+  const[cart,setCart]=useState(10000)
+  const kis=(a)=>{
+    setCart(cart+100)
+  }
+  const ki=(b)=>{
+    setCart(cart-1000)
+  }
+  const k=(c)=>{
+    setCart(cart+200)
+  }
+  const de=(d)=>{
+    setCart(cart-2000)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <p>{cart}</p>
+    <button onClick={(a)=>kis()}>100</button>
+    <button onClick={(b)=>ki()}>-1000</button>
+    <button onClick={(c)=>k()}>200</button>
+    <button onClick={(d)=>de()}>-2000</button>
+
+    
+
     </div>
   );
 }
